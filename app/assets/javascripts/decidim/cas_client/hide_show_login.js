c8724acb28a8a5ffd91f8f1741693f9ceede6671
Víctor <victor.ol@coditramuntana.com>
2020-05-19 16:41:34 +0200

    Change attr to css in order to read attribute properly

--- app/assets/javascripts/decidim/cas_client/hide_show_login.js
+++ app/assets/javascripts/decidim/cas_client/hide_show_login.js
@@ -1,6 +1,5 @@
 const changeVisibilityLoginBox = () => {
   var element = $('.login__box');
-  var first = element.attr('display');
-  var display = element.attr('display') == 'block' ? 'none' : 'block';
+  var display = element.css('display') == 'block' ? 'none' : 'block';
   element.css('display', display);
 };
